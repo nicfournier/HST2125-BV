102c2eccdc647d6690369968e046d3ea6d4aa33e
santiagok986 <39525483+santiagok986@users.noreply.github.com>
2018-07-04 11:27:20 -0400

    Changement Jumbotron
    test jumbo

--- pages/S01/S01-Activity/S01-A01.vue
+++ pages/S01/S01-Activity/S01-A01.vue
@@ -1,39 +1,49 @@
 <template>
     <div>
         <b-jumbotron 
-            bg-variant="https://farm6.staticflickr.com/5564/30027314480_81db2b8384_b.jpg" 
-            header="Titre ?" 
+            bg-variant="https://farm2.staticflickr.com/1744/40757444840_126f6ab628_o.png" 
+            header="Activité 1" 
             lead="" 
             text-variant="white">
         </b-jumbotron>
         <b-container class="">
             <b-row  align-h="end">
                 <b-col>
-                    <h4 class="alert-heading">Activité 1</h4>
-                    <p  class="lead">
+                   
+                    <p class="lead">
                         Je vous souhaite tout d’abord la bienvenue au cours!
                     </p>
                     <hr>
                     <p> 
                     </p>
                     <b-tabs pills card vertical>
-                        <b-tab title="Objectifs" active>
+                        <b-tab title="Consignes générales" active>
+                            <h4>Consignes générales</h4>
                             <p>
                                 L’objectif des ateliers d’initiation est de passer en revue ensemble les différentes activités de la semaine mais aussi de vous donner une initiation aux orientations thématiques de la semaine. Je vous invite à prendre des notes comme pour un cours en classe pour développer avec mon aide une grille thématique de mise en ordre des informations recueillies à travers les différentes activités. Autrement dit, par l’atelier d’initiation, je vous propose une façon de mieux structurer le travail de la semaine.
                             </p>
-                            <p>
+                          
+                        </b-tab>
+                        <b-tab title="Consignes spécifiques" >
+                              <p>
                                 L’atelier d’initiation de la première semaine me permettra d’abord de me présenter à vous et de vous expliquer de vive voix l’organisation et la philosophie pédagogique du cours et de placer déjà quelques jalons spatio-temporels et thématiques de l’époque du haut Moyen Âge.
                             </p>
-                        </b-tab>
-                        <b-tab title="Consignes" >
                             <ol>
-                                <li>D’abord, prenez connaissance du Guide du participant à une classe virtuelle Adobe Connect [Lien vers le document].</li>
+                                <li>D’abord, prenez connaissance du Guide du participant à une <i>classe virtuelle Adobe Connect</i> [Lien vers le document].</li>
                                 <li>À l’heure indiquée dans le plan de cours [Lien vers le plan de cours], connectez-vous à la classe virtuelle Adobe Connect en cliquant sur le lien fourni dans l’espace StudiUM.</li>
                                 <li>N’oubliez pas d’avoir à portée de main l’ensemble de votre matériel.</li>
+                                <li>Ayez pris connaissance du plan de cours, des ressources et des autres éléments importants avant la rencontre et notez vos questions éventuelles dans le forum de l'atelier 1.</li>
+
                             </ol>
                         </b-tab>
                         <b-tab title="Ressources">
-                            <b-card-group deck>
+                            <ul>
+                                <li>Plan de cours</li>
+                                <li>Fiches descriptives des évaluations</li>
+                                <li>Guide du participant à une classe virtuelle Adobe Connect</li>
+                            </ul>
+                            
+                           <!-- <b-card-group deck>
                                 <b-card v-b-modal.modal1 img-alt="Card image" img-top>
                                     <p class="card-text">
                                         Plan de cours
@@ -49,7 +59,7 @@
                                         Guide du participant à une classe virtuelle Adobe Connect
                                     </p>
                                 </b-card>
-                            </b-card-group>
+                            </b-card-group>-->
                         </b-tab>
                     </b-tabs>
                 </b-col>
@@ -100,7 +110,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .jumbotron {
-  background: url(https://farm9.staticflickr.com/8267/29693441973_edba460a5c_b.jpg);
+  background: url(https://farm2.staticflickr.com/1744/40757444840_126f6ab628_o.png);
   background-repeat: no-repeat;
   background-size: cover;
 }
