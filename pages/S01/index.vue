6be39a7229e4603e39a6c10c4b898cf294fd6f93
santiagok986 <39525483+santiagok986@users.noreply.github.com>
2018-07-04 11:45:47 -0400

    Index S01
    Changement index 01

--- pages/S01/index.vue
+++ pages/S01/index.vue
@@ -81,14 +81,14 @@
                   <edActivity>
                     <div slot="content">
                       <b-card-group deck>
-                      <NavigationCard title="Activité 1" icon="record_voice_over" colorType="white" activityLink="/S01-Activity/S01-A01/">
+                      <NavigationCard title="Activité 1" icon="record_voice_over" colorType="white" activityLink="S01-Activity/S01-A01">
                         <h4 slot="actTitle">Atelier d'initiation</h4>
                       </NavigationCard>
                       <NavigationCard icon="forum" title="Activité 2" colorType="white" imgSrc="" activityLink="S01-Activity/S01-A02">
                         <h4 slot="actTitle">Se présenter dans le forum Présentations</h4>
                       </NavigationCard>
                       <NavigationCard icon="remove_red_eye" title="Activité 3" colorType="white" activityLink="S01-Activity/S01-A03">
-                        <h4 slot="actTitle">Se familiariser avec l'organisation du cours et formuler les attentes</h4>
+                        <h4 slot="actTitle">Appropriation de votre portfolio étudiant UdeM</h4>
                       </NavigationCard>
                       <NavigationCard icon="remove_red_eye"  textColor="dark" title="Activité 4" colorType="white" activityLink="S01-Activity/S01-A04">
                         <h4 slot="actTitle">Prendre connaissance des principes de fiche de lecture</h4>
@@ -98,10 +98,14 @@
                       <NavigationCard icon="book" title="Activité 5" colorType="white" textColor="dark" activityLink="S01-Activity/S01-A05">
                         <h4 slot="actTitle">S'initier aux dimensions spatio-temporels à l'aide du module "Initiation au haut Moyen Âge" avec fiche de lecture 1</h4>
                       </NavigationCard>
-                      <NavigationCard icon="remove_red_eye" title="Activité 6" colorType="white" textColor="dark">
+                      <NavigationCard icon="remove_red_eye" title="Activité 6" colorType="white" textColor="dark" activityLink="S01-Activity/S01-A06">
                         <h4 slot="actTitle">Se familiariser avec la typologie des sources du haut Moyen Âge 1</h4>
                       </NavigationCard>
-                      <NavigationCard icon="record_voice_over" title="Activité 7" colorType="white" textColor="dark">
+                      <NavigationCard icon="remove_red_eye" title="Activité 7" colorType="white" textColor="dark" activityLink="S01-Activity/S01-A07">
+                        <h4 slot="actTitle">Comprendre les principes de la critique et de l'analyse de source et sa systématisation dans une fiche de source
+</h4>
+                      </NavigationCard>
+                       <NavigationCard icon="record_voice_over" title="Activité 8" colorType="white" textColor="dark" activityLink="S01-Activity/S01-A08">
                         <h4 slot="actTitle">Atelier d'approfondissement</h4>
                       </NavigationCard>
                   </b-card-group>
@@ -132,7 +136,6 @@ export default {
 </script>-->
 
 <script>
-
 import BadgeIcon from '@/components/BadgeIcon.vue'
 import Steps from '@/components/Steps.vue'
 import Step from '@/components/Step.vue'
@@ -141,7 +144,6 @@ import edObjective from '@/components/edObjective.vue'
 import edEssential from '@/components/edEssential.vue'
 import edIntroduction from '@/components/edIntroduction.vue'
 import edActivity from '@/components/edActivity.vue'
-
 export default {
   layout: 'default',
   components: {
@@ -165,7 +167,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .jumbotron {
-  background: url(https://farm6.staticflickr.com/5564/30027314480_81db2b8384_b.jpg);
+   background: url(https://farm2.staticflickr.com/1744/40757444840_126f6ab628_o.png);
   background-repeat: no-repeat;
   background-size: cover;
 }
